ebc488fe6e997f2175f54f93c2c0cd7c511fb393
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-11-23 21:59:21 +0900

    Feat : UUID 링크생성(수정)

--- src/main/java/com/likelion/letterBox/service/UserLinkService.java
+++ src/main/java/com/likelion/letterBox/service/UserLinkService.java
@@ -19,8 +19,10 @@ public class UserLinkService {
             User user = userRepository.findByEmail(email).orElseThrow(() -> {
                 throw new IllegalArgumentException("유저 정보를 불러올 수 없습니다");
             });
-            user.setOwnerId(UUID.randomUUID());
-            userRepository.save(user);
+            if(user.getOwnerId()==null) {
+                user.setOwnerId(UUID.randomUUID());
+                userRepository.save(user);
+            }
 
             // url 임시 설정 -> 링크를 string형식으로 반환하는게 맞는지 모르겠습니닷 ㅜ
             // 아니면 uuid 자체만 반환하는게 맞을까요?
